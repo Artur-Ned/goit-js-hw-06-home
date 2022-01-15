c39e140bdc5eec5603f1a271e1d8a4a48427e4c9
NNASTYA2007 <advokat1707@gmail.com>
2022-01-15 13:57:50 +0200

    beegintask#2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,33 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+let arrOfIngredients = document.querySelector('#ingredients');
+
+
+
+for (let ingredient of ingredients) {
+  let liIngredient = liIngredient.createElement('li');
+  liIngredient.textContent = "PPPPPPPP";
+  arrOfIngredients.append.liIngredient;
+  // console.log(ingredient);
+}
+
+
+/*
+В HTML есть пустой список ul#ingredients.
+<ul id="ingredients"></ul>
+В JavaScript есть массив строк.
+
+const ingredients = [
+  ,
+];
+Напиши скрипт, который для каждого элемента массива ingredients:
+
+Создаст отдельный элемент <li>. Обзательно используй метод document.createElement().
+Добавит название ингредиента как его текстовое содержимое.
+Добавит элементу класс item.
+После чего вставит все <li> за одну операцию в список ul.ingredients.
+
+
+**/
