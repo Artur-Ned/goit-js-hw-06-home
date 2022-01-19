02c77a02c86420306e8e49da985e8f729b230d93
NNASTYA2007 <advokat1707@gmail.com>
2022-01-19 22:15:02 +0200

    task9done

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,28 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+
+const btnRef = document.querySelector('.change-color');
+const bodyRef = document.querySelector('body');
+const spanRef = document.querySelector('.color');
+
+btnRef.addEventListener('click', changeColor);
+
+function changeColor() {
+  // getRandomHexColor();
+  // let color = getRandomHexColor();
+  bodyRef.style.backgroundColor = getRandomHexColor();
+  spanRef.textContent = getRandomHexColor();
+  
+}
+
+
+/**Напиши скрипт, который изменяет цвета фона элемента <body> через инлайн стиль при клике на button.change-color и выводит значение цвета в span.color.
+
+
+Для генерации случайного цвета используй функцию getRandomHexColor.
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+} */
